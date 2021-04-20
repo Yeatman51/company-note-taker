acd7749e5d32bdc54a2a1e8c8b74ea92e5553708
Jon Yeatman <jonlyeatman@gmail.com>
2021-04-20 11:11:53 -0400

    listening post update

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 https://github.com/Yeatman51
 https://github.com/Yeatman51/team-profile-generator
+https://company-note-taker.herokuapp.com/
 
 ![command-line-prompt.png](assets/command-line-prompt.png)
--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const nanoid = require('nanoid');
 
 // Sets up the Express App
 const app = express();
-const PORT = 3000;
-// const PORT = process.env.PORT || 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
